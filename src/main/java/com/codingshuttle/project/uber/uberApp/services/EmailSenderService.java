1acb290a96c41d73257be7ff4e0fb60fb4584146
Lokesh Kumar <lokesh2.yss@gmail.com>
2024-08-28 20:41:06 +0530

    Added email support in the app

---
+++ src/main/java/com/codingshuttle/project/uber/uberApp/services/EmailSenderService.java
@@ -0,0 +1,6 @@
+package com.codingshuttle.project.uber.uberApp.services;
+
+public interface EmailSenderService {
+    void sendEmail(String toEmail, String subject, String body);
+    void sendEmail(String[] toEmail, String subject, String body);
+}
--- src/test/java/com/codingshuttle/project/uber/uberApp/UberAppApplicationTests.java
+++ src/test/java/com/codingshuttle/project/uber/uberApp/UberAppApplicationTests.java
@@ -1,13 +1,23 @@
 package com.codingshuttle.project.uber.uberApp;
 
+import com.codingshuttle.project.uber.uberApp.services.EmailSenderService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UberAppApplicationTests {
-
+	@Autowired
+	private EmailSenderService emailSenderService;
 	@Test
 	void contextLoads() {
+		emailSenderService.sendEmail("kokogog633@hapied.com", "Hello Lokesh", "Hellolokefkeof");
+	}
+
+	@Test
+	void sendEmailMultiple() {
+		String[] emails = {"kokogog633@hapied.com", "lokesh2.yss@gmail.com"};
+		emailSenderService.sendEmail(emails, "Hello Lokesh", "Hellolokefkeof");
 	}
 
 }
